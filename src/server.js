b3af59ea048265c5c2a737cdb78a977a4bc4f288
davidRyu <david.ryu@imagoworks.ai>
2022-07-30 21:20:32 +0900

    feat: install ws and create ws on http server

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from 'http';
+import WebSocket from 'ws';
 import express from 'express';
 
 const app = express();
@@ -9,5 +11,11 @@ app.use('/public', express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
 app.get('/*', (req, res) => res.redirect('/'))
 
-const handleListen = () => console.log(`Listening on localhost:3000`);
-app.listen(3000, handleListen);
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+// app.listen(3000, handleListen);
+
+// http server + ws server
+const server = http.createServer(app);
+const wss = new WebSocket.Server({server});
+
+server.listen(3000, handleListen);
